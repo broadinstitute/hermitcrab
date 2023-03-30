55ee1aedf8f5d288094026aef361456d990b9fc5
Philip Montgomery <pmontgom@broadinstitute.org>
2023-03-29 22:55:53 -0400

    added poetry project def

--- hermitcrab/command/up.py
+++ hermitcrab/command/up.py
@@ -1,6 +1,6 @@
 from ..ssh import get_pub_key
 from ..gcp import gcloud
-from ..config import get_instance_config
+from ..config import get_instance_config, CONTAINER_SSHD_PORT
 import tempfile
 from ..tunnel import is_tunnel_running, stop_tunnel, start_tunnel
 
@@ -44,7 +44,7 @@ write_files:
     [Service]
     Environment="HOME=/home/cloudservice"
     ExecStartPre=/usr/bin/docker-credential-gcr configure-docker
-    ExecStart=/usr/bin/docker run --rm --name=container-sshd -p{config.CONTAINER_SSHD_PORT}:22 -v /mnt/disks/{instance_config.pd_name}/home/ubuntu:/home/ubuntu {instance_config.docker_image} /usr/sbin/sshd -D -e
+    ExecStart=/usr/bin/docker run --rm --name=container-sshd -p{CONTAINER_SSHD_PORT}:22 -v /mnt/disks/{instance_config.pd_name}/home/ubuntu:/home/ubuntu {instance_config.docker_image} /usr/sbin/sshd -D -e
     ExecStop=/usr/bin/docker stop container-sshd
     ExecStopPost=/usr/bin/docker rm container-sshd
 runcmd:
