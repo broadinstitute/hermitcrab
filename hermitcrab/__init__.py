cdb43b1f8b74f170d4374c18eb15eb05e2c98477
Philip Montgomery <pmontgom@broadinstitute.org>
2023-05-03 10:39:17 -0400

    fix for hermit version

--- hermitcrab/__init__.py
+++ hermitcrab/__init__.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
 try:
-    __version__ = importlib.metadata.version("hermit")
+    __version__ = importlib.metadata.version("hermitcrab")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
