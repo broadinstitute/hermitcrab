1a2bcd1a4615473b0249facbfd5d7f52832c43d3
Philip Montgomery <pmontgom@broadinstitute.org>
2023-05-06 21:11:12 -0400

    fixed test

--- tests/hermitcrab/test_end_to_end.py
+++ tests/hermitcrab/test_end_to_end.py
@@ -1,10 +1,22 @@
-
 from hermitcrab.main import main
 
+
 def test_end_to_end(vcr, tmphomedir):
-    main(["create", "--project", "broad-achilles", "--zone", 
-          "us-central1-a", "hermit-demo", "50", 
-          "us.gcr.io/broad-achilles/hermitcrab:with-docker", "--boot-disk-size", "55"])
+    main(
+        [
+            "create",
+            "--project",
+            "broad-achilles",
+            "--zone",
+            "us-central1-a",
+            "hermit-demo",
+            "--disk-size",
+            "50",
+            "us.gcr.io/broad-achilles/hermitcrab:with-docker",
+            "--boot-disk-size",
+            "55",
+        ]
+    )
     main(["status"])
     main(["up"])
     main(["status"])
